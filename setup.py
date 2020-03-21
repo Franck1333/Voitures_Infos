fd402783179fc18ff186269fd060279a27821c5d
Franck1333 <fun13@live.fr>
2020-03-21 20:29:49 +0100

    Install settings
    Install settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settingsInstall settings

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-#Aides : Using set-me-up.py from https://gist.github.com/ojii/3906682 ,
 #Aides : The generator "set-me-up.py" need a "__init__.py" file in the main root directorie to work .
-
 #HOW INSTALL AND USE THIS PROJECT:
 #in the console : sudo python setup.py install
 #And all the depencies will be installed with the Project
+#Listing env> sudo pip freeze > list.txt
 
-#git clone https://github.com/Franck1333/ProjetBrassard.git
+#git clone https://github.com/Franck1333/Voitures_Infos.git
 
 from setuptools import setup, find_packages
 
 setup(
-    name='ProjetBrassard',
+    name='Voitures_Infos',
     version="1.13",
     author='Franck Rochat',
     author_email='rochat.franck@gmail.com',
-    description='This Project give you useful details about the environement where you are by Using an USB GPS and some API with an Internet Connection',
-    url='https://github.com/Franck1333/ProjetBrassard',
+    description='This software got you some interesting infos when you go somewhere.',
+    url='https://github.com/Franck1333/Voitures_Infos',
     license='lgpl',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["geopy==1.18.1","pyowm==2.9","unidecode","unirest"], #Get the Dependencies from Pypi (pip install)
-    dependency_links=['https://github.com/pimoroni/hyperpixel.git','https://github.com/csparpa/pyowm.git'], #Get the Dependencies via HTTP(s)
+    install_requires=["pyserial==3.2.1","geopy==1.20.0","Kivy==1.11.1","matplotlib==3.0.3","numpy==1.17.1","pandas==0.25.1","pynmea2==1.15.0","pyowm==2.9.0","requests==2.22.0","Unidecode==1.1.1","urllib3==1.25.6","wifi==0.3.8"], #Get the Dependencies from Pypi (pip install)
+    dependency_links=[], #Get the Dependencies via HTTP(s)
 )
