82001ebe7b5d80dae61d3be7de28612542b2c437
Franck1333 <fun13@live.fr>
2020-06-05 17:31:54 +0200

    Stop WebInterface
    OKKKKKKKKKKKKK....
    
    I stop to work on it , 'cause i've lost too much of my time with that....
    Why things are so complicated $@#~!!!!

--- Web_Interface/NodeJS/views/static/js/francky.js
+++
@@ -1,6 +0,0 @@
-function update_values() {
-            $.get("/",out).done(
-                function(data) {
-                    $("#system_time").text(data.system_time)
-                });
-        }
